af2187fa0729400d48d6d15c98c1902328c89b0b
psaporito <107567639+psaporito@users.noreply.github.com>
2024-12-16 17:36:55 +0100

    - case base & array

--- src/Song99Bottles.ts
+++ src/Song99Bottles.ts
@@ -1,15 +1,39 @@
 class Song99Bottles {
-    song(startNumber: number,length: number): string[] {
+    song(startNumber: number, length: number): string[] {
 
         const array_result: string[] = [];
-        for(let i=0; i< length; i++){
-            const bottles= startNumber - i;
+        for (let i = 0; i < length; i++) {
+            const bottles = startNumber -i;
             const bottles_next = bottles - 1;
-            if(bottles >= 2 )
-                array_result.push(`${bottles} bottles of beer on the wall, ${bottles} ${bottles >= 2 ? 'bottles' : 'bottle'} of beer. Take one down and pass it around, ${bottles_next} ${bottles_next >= 2 ? 'bottles' : 'bottle'} of beer on the wall.`);
+            array_result.push(`${this.bottlesOnTheWall(bottles)}, ${this.bottlesOfBeer(bottles)}. ${this.toTheStore(bottles_next)}, ${this.bottlesOnTheWall(bottles_next)}.`);
         }
 
         return array_result;
+    }
+
+    bottlesOnTheWall(bottles: number) {
+        return `${this.bottlesOfBeer(bottles)} on the wall`
+    }
+
+    bottlesOfBeer(bottles: number) {
+        if (bottles === 1) {
+            return `${bottles} bottle of beer`
+        }
+        if (bottles === 0) {
+            return 'no more bottles of beer';
+        }
+        if(bottles === -1) {
+            return `99 bottles of beer`
+        }
+
+        return `${bottles} bottles of beer`
+    }
+
+    toTheStore(bottles: number) {
+        if (bottles > -1) {
+            return 'Take one down and pass it around';
+        }
+        return 'Go to the store and buy some more';
     }
 }
 
