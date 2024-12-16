f6ae15d7098e07b89ad24ef904436fbb31c2b941
psaporito <107567639+psaporito@users.noreply.github.com>
2024-12-16 15:22:32 +0100

    first commit

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,6 +1,14 @@
+/*
+Write a program that prints all the numbers from 1 to 100, but with some exceptions:
+
+for all numbers that are multiples of 3, print “Fizz” instead of the number itself
+for all numbers that are multiples of 5, print “Buzz” instead of the number itself
+for all numbers that are multiples of both 3 and 5, print “FizzBuzz” instead of the number itself
+ */
 class FizzBuzz {
-    handle(): boolean {
-        return true;
+
+    handle(): string {
+        return 'Fizz';
     }
 }
 
--- test/fizzBuzz.test.ts
+++ test/fizzBuzz.test.ts
@@ -1,14 +1,22 @@
 import FizzBuzz from "../src/fizzBuzz";
 const FizzBuzzClass = new FizzBuzz();
 
-describe('sample test set', function (): void {
+describe('fizzBuzz', function (): void {
 
-  it('sample test', (): void => {
+  /**
+   * Casi da gestire
+   *  0,1,2 => return 0,1,2
+   *  3,6,9 => return Fizz
+   *  5,10,20 => return Buzz
+   *  15,30,45 => return fizzBuzz
+   *  99 => return Fizz
+   *  100 => return Buzz
+   */
+  it('fizz', (): void => {
 
-    var result: boolean = FizzBuzzClass.handle();
+    var result: string = FizzBuzzClass.handle();
 
-    var expected = true;
-    expect(result).toBe(expected);
+    expect(result).toBe('Fizz');
   });
 
 });
